72fd3814efdbeee12a3762f03fcb9c88b06f5cfe
jhonnyV-V <jhonny_varela_visbal@hotmail.com>
2024-12-19 23:25:47 -0400

    executing actions?

--- lua/telescope/_extensions/phoemux/main.lua
+++ lua/telescope/_extensions/phoemux/main.lua
@@ -1,13 +1,51 @@
 local has_telescope = pcall(require, "telescope")
 if not has_telescope then
-  error("This plugins requires nvim-telescope/telescope.nvim")
+	error("This plugins requires nvim-telescope/telescope.nvim")
 end
 -- telescope modules
 local actions = require("telescope.actions")
+local actions_state = require("telescope.actions.state")
 local pickers = require("telescope.pickers")
 local themes = require "telescope.themes"
+local finders = require("telescope.finders")
+local conf = require("telescope.config").values
 
-local phoemux_pickers = require("telescope._extensions.project.finders")
+local open = function(prompt_bufnr)
+	return function()
+		actions.close(prompt_bufnr)
+		local selection = actions_state.get_selected_entry()
+		print(selection.value)
+		os.execute("phoemux " .. selection.value)
+	end
+end
+
+
+local function ash_finder(opts)
+	opts = opts or {}
+	opts.cwd = opts.cwd or vim.fs.normalize(vim.fn.stdpath('config') .. "/../phoemux")
+	local results = {}
+	-- local command = "ls " .. opts.cwd .. " -1 -I cache | cut -d . -f1"
+	-- vim.fs.dir(opts.cwd)
+
+	for name, type in vim.fs.dir(opts.cwd) do
+		if type == "file" and name ~= "cache" then
+			local cleanName = string.gsub(name, ".yaml", "")
+			table.insert(results, cleanName)
+		end
+	end
+
+	return finders.new_table({
+		results = results,
+		cwd = opts.cwd,
+		entry_maker = function(result)
+			return {
+				value = result,
+				display = result,
+				ordinal = result,
+			}
+		end
+	})
+end
 
 local M = {}
 
@@ -17,35 +55,33 @@ local cwd
 local theme_opts = {}
 
 M.setup = function(setup_config)
-  if setup_config.theme and setup_config.theme ~= "" then
-    theme_opts = themes["get_" .. setup_config.theme]()
-  end
-  if setup_config.cwd then
-    cwd = setup_config.cwd
-  end
+	if setup_config.theme and setup_config.theme ~= "" then
+		theme_opts = themes["get_" .. setup_config.theme]()
+	end
+	if setup_config.cwd then
+		cwd = setup_config.cwd
+	end
 end
 
 -- This creates a picker with a list of all of the projects
 M.phoemux = function(opts)
-  opts = vim.tbl_deep_extend("force", theme_opts, opts or {})
-  if cwd then
-    opts.cwd = cwd
-  end
+	opts = vim.tbl_deep_extend("force", theme_opts, opts or {})
+	if cwd then
+		opts.cwd = cwd
+	end
 
-  pickers.new(opts, {
-    prompt_title = 'Find Phoemux Ash',
-    finder = phoemux_pickers.ash_finder(opts),
-    sorter = require("telescope.sorters").empty(),
-    attach_mappings = function(prompt_bufnr)
-      actions.select_default:replace(function()
-        actions.close(prompt_bufnr)
-        local selection = actions.state.get_selected_entry()
-        print(vim.inspect(selection))
-        os.execute("phoemux " .. selection)
-      end)
-      return true
-    end
-  }):find()
+	pickers.new(opts, {
+		prompt_title = 'Find Phoemux Ash',
+		finder = ash_finder(opts),
+		sorter = conf.generic_sorter(opts),
+		attach_mappings = function(prompt_bufnr, map)
+			actions.select_default:replace(open(prompt_bufnr))
+
+			map('n', '<c-o>', open(prompt_bufnr))
+			map('i', '<c-o>', open(prompt_bufnr))
+			return true
+		end
+	}):find()
 end
 
 M.phoemux()
