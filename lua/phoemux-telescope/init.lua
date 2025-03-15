55286474e76a16d3df5e4d417ae5f3f6f9a6826b
jhonnyV-V <jhonny_varela_visbal@hotmail.com>
2025-03-15 16:10:52 -0400

    feature: multiple new pickers

--- lua/phoemux-telescope/init.lua
+++ lua/phoemux-telescope/init.lua
@@ -1,7 +1,7 @@
 local has_telescope, telescope = pcall(require, 'telescope')
 
 if not has_telescope then
-  error('This plugins requires nvim-telescope/telescope.nvim')
+	error('This plugins requires nvim-telescope/telescope.nvim')
 end
 
 -- telescope modules
@@ -12,12 +12,80 @@ local themes = require "telescope.themes"
 local finders = require("telescope.finders")
 local conf = require("telescope.config").values
 
-local open = function(prompt_bufnr)
+local SWITCH = "switch-session"
+local KILL_AND_SWITCH = "SWITCH_AND_KILL"
+local KILL_AND_TARGET = "SWITCH_AND_TARGET"
+
+---@param str string
+---@return Array<string>
+local function split(str)
+	local lines = {}
+	local current_line = ""
+
+	for i = 1, #str do
+		local char = str:sub(i, i)
+		if char == "\n" then
+			table.insert(lines, current_line)
+			current_line = ""
+		elseif char == "\r" then
+			-- handle windows style line endings, skip the \r if \n follows
+			if str:sub(i + 1, i + 1) ~= "\n" then
+				table.insert(lines, current_line)
+				current_line = ""
+			end
+		else
+			current_line = current_line .. char
+		end
+	end
+
+	-- Add the last line if it's not empty
+	if #current_line > 0 then
+		table.insert(lines, current_line)
+	end
+
+	return lines
+end
+
+local function get_sessions()
+	---@type Array<string>
+	local sessions = {}
+	local handle = io.popen("tmux list-sessions -F \"#{session_name}\"")
+	if handle == nil then
+		return
+	end
+	---@type string
+	local result = handle:read("*a")
+	handle:close()
+	sessions = split(result)
+	return sessions
+
+end
+
+---@param prompt_bufnr number
+---@param action string
+local open = function(prompt_bufnr, action)
+	local command = ""
+	if action == SWITCH then
+		command = "tmux switch-client -t"
+	elseif action == KILL_AND_SWITCH then
+		command = "phoemux kill -a"
+	elseif action == KILL_AND_TARGET then
+		command = "phoemux kill -t"
+	else
+		command = "phoemux"
+	end
+
 	return function()
 		actions.close(prompt_bufnr)
 		local selection = actions_state.get_selected_entry()
-		print(selection.value)
-		os.execute("phoemux " .. selection.value)
+		local v = ""
+
+		if action == SWITCH then
+			v = "\"" .. selection.value .. "\""
+		else
+			v = selection.value
+		end
+		os.execute(command .. " " .. v)
 	end
 end
 
@@ -49,6 +117,30 @@ local function ash_finder(opts)
 	})
 end
 
+local function session_finder(opts)
+	local results = get_sessions()
+
+	return finders.new_table({
+		results = results,
+		cwd = opts.cwd,
+		---@param result string
+		entry_maker = function(result)
+			return {
+				value = result,
+				display = result:gsub("%s+", ""),
+				ordinal = result,
+			}
+		end
+	})
+end
+
+
+---@class Phoemux
+---@field phoemux function
+---@field switch_tmux_session function
+---@field kill_and_switch_session function
+---@field kill_session function
+---@field kill_current_session function
 local M = {}
 
 -- Variables that setup can change
@@ -77,17 +169,82 @@ M.phoemux = function(opts)
 		finder = ash_finder(opts),
 		sorter = conf.generic_sorter(opts),
 		attach_mappings = function(prompt_bufnr, map)
-			actions.select_default:replace(open(prompt_bufnr))
+			actions.select_default:replace(open(prompt_bufnr, ""))
 
-			map('n', '<c-o>', open(prompt_bufnr))
-			map('i', '<c-o>', open(prompt_bufnr))
+			map('n', '<c-o>', open(prompt_bufnr, ""))
+			map('i', '<c-o>', open(prompt_bufnr, ""))
 			return true
 		end
 	}):find()
 end
 
-telescope.register_extension{
-  exports = { phoemux = M.phoemux }
+telescope.register_extension {
+	exports = { phoemux = M.phoemux }
 }
+
+M.switch_tmux_session = function(opts)
+	opts = vim.tbl_deep_extend("force", theme_opts, opts or {})
+	if cwd then
+		opts.cwd = cwd
+	end
+
+	pickers.new(opts, {
+		prompt_title = 'Find Tmux Session',
+		finder = session_finder(opts),
+		sorter = conf.generic_sorter(opts),
+		attach_mappings = function(prompt_bufnr, map)
+			actions.select_default:replace(open(prompt_bufnr, SWITCH))
+
+			map('n', '<c-o>', open(prompt_bufnr, SWITCH))
+			map('i', '<c-o>', open(prompt_bufnr, SWITCH))
+			return true
+		end
+	}):find()
+end
+
+M.kill_and_switch_session = function(opts)
+	opts = vim.tbl_deep_extend("force", theme_opts, opts or {})
+	if cwd then
+		opts.cwd = cwd
+	end
+
+	pickers.new(opts, {
+		prompt_title = 'Kill And Switch Tmux Session',
+		finder = session_finder(opts),
+		sorter = conf.generic_sorter(opts),
+		attach_mappings = function(prompt_bufnr, map)
+			actions.select_default:replace(open(prompt_bufnr, KILL_AND_SWITCH))
+
+			map('n', '<c-o>', open(prompt_bufnr, KILL_AND_SWITCH))
+			map('i', '<c-o>', open(prompt_bufnr, KILL_AND_SWITCH))
+			return true
+		end
+	}):find()
+end
+
+M.kill_session = function(opts)
+	opts = vim.tbl_deep_extend("force", theme_opts, opts or {})
+	if cwd then
+		opts.cwd = cwd
+	end
+
+	pickers.new(opts, {
+		prompt_title = 'Kill And Switch Tmux Session',
+		finder = session_finder(opts),
+		sorter = conf.generic_sorter(opts),
+		attach_mappings = function(prompt_bufnr, map)
+			actions.select_default:replace(open(prompt_bufnr, KILL_AND_TARGET))
+
+			map('n', '<c-o>', open(prompt_bufnr, KILL_AND_TARGET))
+			map('i', '<c-o>', open(prompt_bufnr, KILL_AND_TARGET))
+			return true
+		end
+	}):find()
+end
+
+M.kill_current_session = function()
+	os.execute("phoemux kill")
+end
+
 
 return M
